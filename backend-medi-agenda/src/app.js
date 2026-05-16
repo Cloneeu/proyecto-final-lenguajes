46936443fcec300b27f60d35a538e9296fed4e7b
Eladio1234 <luiseladio452@gmail.com>
2026-05-15 21:31:32 -0600

    Guardando cambios en app.js

--- backend-medi-agenda/src/app.js
+++ backend-medi-agenda/src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { env } from './config/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import appointmentsRouter from './modules/appointments/appointments.routes.js';
+import prescriptionsRouter from './modules/prescriptions/prescriptions.routes.js';
 import authRoutes from './modules/auth/auth.routes.js';
 import patientsRouter from './modules/patients/patients.routes.js';
 
@@ -17,6 +19,8 @@ app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 
 // Aqui van las rutas de la API, cada una debe tener su router
 app.use('/api/auth', authRoutes);
+app.use('/api/appointments', appointmentsRouter);
+app.use('/api/prescriptions', prescriptionsRouter);
 app.use('/api/patients', patientsRouter);
 
 // El middleware de manejo de errores va al final para atrapar cualquier error que haya ocurrido en las rutas anteriores
