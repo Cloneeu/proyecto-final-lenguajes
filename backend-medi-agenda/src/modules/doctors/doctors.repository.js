a72449b540e758661582724ed940d99fb6c3c8e1
Juan Pablo Hernández Martínez <jphm.hernandezmartinez@gmail.com>
2026-05-31 15:16:09 -0600

    [FIXED]:TRAE LOS DOCTORES REEALES

--- backend-medi-agenda/src/modules/doctors/doctors.repository.js
+++ backend-medi-agenda/src/modules/doctors/doctors.repository.js
@@ -2,27 +2,32 @@ import { db } from '../../config/firebase.js';
 
 export const doctorsRepository = {
     findAll: async () => {
-        const snapshot = await db.collection('doctors').get();
+        // se filtra en users solo  los que son doctores 
+        const snapshot = await db.collection('users').where('role', '==', 'doctor').get();
         return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     },
 
     create: async (doctorData) => {
         const newDoctor = {
             ...doctorData,
+            role: 'doctor', 
             status: 'active',
             createdAt: new Date().toISOString()
         };
-        const docRef = await db.collection('doctors').add(newDoctor);
+        const docRef = await db.collection('users').add(newDoctor);
         return { id: docRef.id, ...newDoctor };
     },
 
     findById: async (id) => {
-        const doc = await db.collection('doctors').doc(id).get();
-        return doc.exists ? { id: doc.id, ...doc.data() } : null;
+        const doc = await db.collection('users').doc(id).get();
+        return (doc.exists && doc.data().role === 'doctor') // existes? , es doctor ?
+            ? { id: doc.id, ...doc.data() } 
+            : null;
     },
 
     updateStatus: async (id, newStatus) => {
-        await db.collection('doctors').doc(id).update({ status: newStatus });
+        //a ctualizamos el estatus dentro de 'users'
+        await db.collection('users').doc(id).update({ status: newStatus });
         return { id, status: newStatus };
     }
 };
